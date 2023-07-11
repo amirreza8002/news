14808f4d2a1398558aefc9bb5b36e53d2f9a8d35
amirreza <amir.rsf1380@gmail.com>
2023-07-11 12:23:56 +0330

    bootstrap(complete)

--- news/settings.py
+++ news/settings.py
@@ -37,8 +37,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # local
     "accounts.apps.AccountsConfig",
     "pages.apps.PagesConfig",
+    # 3rd party package
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
 
 MIDDLEWARE = [
@@ -130,3 +134,6 @@ AUTH_USER_MODEL = "accounts.CustomUser"
 # login logout redirect setting
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
